6d8eab6d438ce0e160e0ceacc74005b5635b800d
Adrian Valenzuela <adrianvalenz.web@gmail.com>
2022-06-10 18:51:51 -0700

    mobile and dropdown menu working

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,5 +1,8 @@
 import "index.css"
 
+import Swup from 'swup';
+const swup = new Swup(); // only this line when included with script tag
+
 // Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
 
