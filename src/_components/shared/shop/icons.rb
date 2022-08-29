00f9a3deb2cd472958a5a10f48f9b12c05b5c884
Adrian Valenzuela <adrianvalenz.web@gmail.com>
2022-08-29 12:25:58 -0700

    updated social links

--- src/_components/shared/shop/icons.rb
+++ src/_components/shared/shop/icons.rb
@@ -1,5 +1,5 @@
 class Shared::Shop::Icons < Bridgetown::Component
-  def initialize(shop: nil, icons: false)
-    @shop, @icons = shop, icons
+  def initialize(shop: nil, links:, icons: false)
+    @shop, @links, @icons = shop, links, icons
   end
 end
