7cb8f6154a4cbb18cd631255d0fd733dcd441349
Adrian Valenzuela <adrianvalenz.web@gmail.com>
2022-09-03 18:34:14 -0700

    dynamic date with JavaScript on footer

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -5,3 +5,5 @@ import "syntax-highlighting.css"
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
 
 console.info("Bridgetown is loaded!")
+
+document.getElementById("year").innerHTML = new Date().getFullYear(); // Alway have current year in footer.
